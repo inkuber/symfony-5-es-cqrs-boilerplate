42ccc9cf628b7dabdc5e20b246ed947d8cc67dc6
Temir Umurzakov <temir@umurzakov.com>
2021-03-10 23:11:18 +0600

    FIX: UserTest

--- tests/unit/Domain/User/UserTest.php
+++ tests/unit/Domain/User/UserTest.php
@@ -1,6 +1,7 @@
 <?php namespace App\Tests\Domain\User;
 
 use App\Auth\Domain\User\User;
+use App\Auth\Domain\User\UserId;
 use App\Auth\Domain\User\ValueObject\Auth\Credentials;
 use App\Auth\Domain\User\ValueObject\Auth\HashedPassword;
 use App\Auth\Domain\User\ValueObject\Email;
@@ -26,14 +27,14 @@ class UserTest extends \Codeception\Test\Unit
     public function testShouldCreateUser()
     {
         $user = User::create(
-            $uuid = Uuid::uuid4(),
+            UserId::fromString($uuid = Uuid::uuid4()->__toString()),
             $credentials = new Credentials(
                 UniqueEmail::fromEmail(Email::fromString("a@a.a")),
                 HashedPassword::encode("aaaaaa")
             )
         );
 
-        $this->assertSame($uuid, $user->uuid());
+        $this->assertSame($uuid, $user->id()->__toString());
         $this->assertSame($credentials, $user->credentials());
     }
 }
